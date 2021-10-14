373a70387e10941765bbdc7e851654938ec1194f
James White <xjwhite@r2ls.com>
2021-10-14 21:57:16 +0200

    1.0.15+515 - production release

--- ios/Flutter/flutter_export_environment.sh
+++ ios/Flutter/flutter_export_environment.sh
@@ -1,15 +1,13 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
-export "FLUTTER_ROOT=/Users/jameswhite/Desktop/Development.nosync/flutter"
+export "FLUTTER_ROOT=/users/jameswhite/desktop/Development.nosync/flutter"
 export "FLUTTER_APPLICATION_PATH=/Users/jameswhite/Desktop/Development.nosync/Projects/HC/HarrierCentralMobile-Flutter"
 export "COCOAPODS_PARALLEL_CODE_SIGN=true"
-export "FLUTTER_TARGET=/Users/jameswhite/Desktop/Development.nosync/Projects/HC/HarrierCentralMobile-Flutter/lib/main.dart"
+export "FLUTTER_TARGET=lib/main.dart"
 export "FLUTTER_BUILD_DIR=build"
 export "FLUTTER_BUILD_NAME=1.0.15"
 export "FLUTTER_BUILD_NUMBER=515"
-export "DART_DEFINES=Zmx1dHRlci5pbnNwZWN0b3Iuc3RydWN0dXJlZEVycm9ycz10cnVl,RkxVVFRFUl9XRUJfQVVUT19ERVRFQ1Q9dHJ1ZQ=="
 export "DART_OBFUSCATION=false"
-export "EXTRA_FRONT_END_OPTIONS=--no-sound-null-safety"
-export "TRACK_WIDGET_CREATION=true"
+export "TRACK_WIDGET_CREATION=false"
 export "TREE_SHAKE_ICONS=false"
 export "PACKAGE_CONFIG=/Users/jameswhite/Desktop/Development.nosync/Projects/HC/HarrierCentralMobile-Flutter/.dart_tool/package_config.json"
