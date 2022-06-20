0e365cd887df666e479571b7006e6d02c0adb16f
jameswhite <gd@jamesawhite.com>
2022-06-20 20:20:50 +0200

    Int: Super admin can be controlled on the app

--- lib/pages/kennel_admin/app_access_page.dart
+++ lib/pages/kennel_admin/app_access_page.dart
@@ -22,55 +22,61 @@ class _AppAccessPageState extends State<AppAccessPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          centerTitle: true,
-          backgroundColor: themeAppBarBackground,
-          title: const Text(
-            'Set HC App access',
-            style: TextStyle(
-              color: Colors.white,
-            ),
+      appBar: AppBar(
+        centerTitle: true,
+        backgroundColor: themeAppBarBackground,
+        title: const Text(
+          'Set HC App access',
+          style: TextStyle(
+            color: Colors.white,
           ),
         ),
-        body: appAccess == null
-            ? Container()
-            : SingleChildScrollView(
-                child: Container(
-                  padding: const EdgeInsets.only(top: 25.0, left: 25.0, right: 25.0, bottom: 70.0),
-                  decoration: Backgrounds.defaultHcBackgroundLight(),
-                  child: Column(
-                    children: <Widget>[
-                      // getOption('Is on mismanagement', mmRoles.getMismanagementState(mmRoleIsOnMm), (bool value) {
-                      //   mmRoles.setMismanagementState(mmRoleIsOnMm, value);
-                      // }),
-                      getOption('Manage Kennel(s)', appAccess.getAppAccess(authCanManageKennel), (bool value) {
-                        appAccess.setAppAccess(authCanManageKennel, value);
-                      }),
-                      getOption('Manage Runs', appAccess.getAppAccess(authCanManageRuns), (bool value) {
-                        appAccess.setAppAccess(authCanManageRuns, value);
-                      }),
-                      getOption('Manage Hash Cash', appAccess.getAppAccess(authCanManageHashCash), (bool value) {
-                        appAccess.setAppAccess(authCanManageHashCash, value);
-                      }),
-                      getOption('Manage Members', appAccess.getAppAccess(authCanManageMembers), (bool value) {
-                        appAccess.setAppAccess(authCanManageMembers, value);
-                      }),
+      ),
+      body: appAccess == null
+          ? Container()
+          : Container(
+              padding: const EdgeInsets.only(top: 25.0, left: 25.0, right: 25.0, bottom: 70.0),
+              decoration: Backgrounds.defaultHcBackgroundLight(),
+              height: MediaQuery.of(context).size.height,
+              child: SingleChildScrollView(
+                child: Column(
+                  children: <Widget>[
+                    // getOption('Is on mismanagement', mmRoles.getMismanagementState(mmRoleIsOnMm), (bool value) {
+                    //   mmRoles.setMismanagementState(mmRoleIsOnMm, value);
+                    // }),
 
-                      ElevatedButton(
-                        style: ElevatedButton.styleFrom(
-                          primary: Colors.red.shade900,
-                        ),
-                        onPressed: () {
-                          // if appAccessFlags = 1 that means this person has no admin privileges, so set all to zero while at the same time preserving the superAdmin bit, otherwise set the authIsAdmin flag
-                          final int access = (appAccess.appAccessFlags & authAllFlags) <= 1 ? (appAccess.appAccessFlags & authIsSuperAdmin) : appAccess.appAccessFlags | authIsAdmin;
-                          Navigator.of(context).pop(access);
-                        },
-                        child: Text('Save changes', style: textStyleButton),
+                    getOption('Is Super Admin', appAccess.getAppAccess(authIsSuperAdmin), (bool value) {
+                      appAccess.setAppAccess(authIsSuperAdmin, value);
+                    }),
+                    getOption('Manage Kennel(s)', appAccess.getAppAccess(authCanManageKennel), (bool value) {
+                      appAccess.setAppAccess(authCanManageKennel, value);
+                    }),
+                    getOption('Manage Runs', appAccess.getAppAccess(authCanManageRuns), (bool value) {
+                      appAccess.setAppAccess(authCanManageRuns, value);
+                    }),
+                    getOption('Manage Hash Cash', appAccess.getAppAccess(authCanManageHashCash), (bool value) {
+                      appAccess.setAppAccess(authCanManageHashCash, value);
+                    }),
+                    getOption('Manage Members', appAccess.getAppAccess(authCanManageMembers), (bool value) {
+                      appAccess.setAppAccess(authCanManageMembers, value);
+                    }),
+
+                    ElevatedButton(
+                      style: ElevatedButton.styleFrom(
+                        primary: Colors.red.shade900,
                       ),
-                    ],
-                  ),
+                      onPressed: () {
+                        // if appAccessFlags = 1 that means this person has no admin privileges, so set all to zero while at the same time preserving the superAdmin bit, otherwise set the authIsAdmin flag
+                        final int access = (appAccess.appAccessFlags & authAllFlags) <= 1 ? (appAccess.appAccessFlags & authIsSuperAdmin) : appAccess.appAccessFlags | authIsAdmin;
+                        Navigator.of(context).pop(access);
+                      },
+                      child: Text('Save changes', style: textStyleButton),
+                    ),
+                  ],
                 ),
-              ));
+              ),
+            ),
+    );
   }
 
   Widget getOption(String title, bool value, Function toggleState) {
