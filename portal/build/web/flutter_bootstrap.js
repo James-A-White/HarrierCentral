e9b56d61fd8f61a9e3bc2078969b7ad3ef98b9e3
James White <jgd@jamesawhite.com>
2026-05-13 20:53:51 +0100

    1.4.5+615: filter unlocated runs from location lookup, SP syncLat/syncLong fix

--- portal/build/web/flutter_bootstrap.js
+++ portal/build/web/flutter_bootstrap.js
@@ -33,6 +33,10 @@ addEventListener("message", eventListener);
 if (!window._flutter) {
   window._flutter = {};
 }
-_flutter.buildConfig = {"engineRevision":"42d3d75a56efe1a2e9902f52dc8006099c45d937","builds":[{"compileTarget":"dart2js","renderer":"canvaskit","mainJsPath":"main.dart.js"}]};
+_flutter.buildConfig = {"engineRevision":"42d3d75a56efe1a2e9902f52dc8006099c45d937","builds":[{"compileTarget":"dart2js","renderer":"canvaskit","mainJsPath":"main.dart.js"},{}]};
 
-_flutter.loader.load();
+_flutter.loader.load({
+  serviceWorkerSettings: {
+    serviceWorkerVersion: "2765161844" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+  }
+});
